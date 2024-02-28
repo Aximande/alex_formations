1927b2a7c01ce180083c3b5dec4bdc07b457a31b
Alexandre Lavallee <lavallee.alexx@gmail.com>
2024-02-28 02:00:31 +0100

    Update 02_Dall-E_🎨.py

--- pages/02_Dall-E_🎨.py
+++ pages/02_Dall-E_🎨.py
@@ -3,6 +3,15 @@ from PIL import Image
 from utils.images_generator import generate_image_openai
 
 
+left_co, cent_co, last_co = st.columns(3)
+with cent_co:
+    st.image(
+        Image.open("static/pathe-logo-clean-PhotoRoom.png"),
+        width=200,
+    )
+
+st.markdown("# Dall-e 🎨")
+
 
 # we create a conversation with the user to create images using function generate_image_openai
 input_text = st.text_input("Decrivez l'image que vous souhaitez générer")
