2257df7884857fe08c1707141c81b0d085b48b59
Alexandre Lavallee <lavallee.alexx@gmail.com>
2024-03-29 00:17:17 +0100

    Update Accueil.py

--- Accueil.py
+++ Accueil.py
@@ -6,16 +6,16 @@ st.set_page_config(page_title="Accueil")
 left_co, cent_co, last_co = st.columns(3)
 with cent_co:
     st.image(
-        Image.open("static/pathe-logo-clean-PhotoRoom.png"),
+        Image.open("static/campus-logo-white.png"),
         width=200,
     )
 
 st.title("Accueil")
-st.write("Ad's up campus: Bienvenue sur notre application de démonstration !")
+st.write("Ad's up campus: Bienvenu(e)s sur notre application de démonstration !")
 st.write("Cette application est divisée en 3 pages :")
 st.write("1. Accueil")
 st.write("2. Chatbot 🤖")
 st.write("3. Dall-E 🎨")
 st.write("Vous pouvez accéder à ces pages via le menu de gauche.")
 st.write("Pour commencer, vous pouvez vous rendre sur la page Chatbot.")
-st.write("Bonne visite !")
+st.write("A vos prompts ! !")
