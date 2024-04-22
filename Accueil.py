b59669aecf9af5f8da4652867b49bc398dc75281
alexandre lavallee <lavallee.alexx@gmail.com>
2024-04-22 19:58:15 +0200

    update accueil

--- Accueil.py
+++ Accueil.py
@@ -6,16 +6,17 @@ st.set_page_config(page_title="Accueil")
 left_co, cent_co, last_co = st.columns(3)
 with cent_co:
     st.image(
-        Image.open("static/campus-logo-white.png"),
+        Image.open("static/brutAI_logo_noir_background.png"),
         width=200,
     )
 
-st.title("Accueil")
-st.write("Ad's up campus: Bienvenu(e)s sur notre application de démonstration !")
-st.write("Cette application est divisée en 3 pages :")
+st.title("Welcome Lunch & Learn BrutAI 🤖")
+st.write("Ad's up campus: Bienvenu(e)s sur notre application de démo pour ces lunch and learn dédiés à l'IA générative !")
+st.write("Cette application est divisée en différentes pages, chacune illustrant un cas d'application particulier de l'IA générative grâce à des chatGPT (GPT-4) personnalisées pour les besoins de notre session :")
 st.write("1. Accueil")
-st.write("2. Chatbot 🤖")
-st.write("3. Dall-E 🎨")
+st.write("2. Chattez avec Brutus, l'assistant GPT-4 pouvant interagir avec un PDF 🤖")
+st.write("3. Créez des images avec Dall-E 3 🎨")
+st.write("4. Chattez avec n'importe quel site web facilement ")
 st.write("Vous pouvez accéder à ces pages via le menu de gauche.")
-st.write("Pour commencer, vous pouvez vous rendre sur la page Chatbot.")
+st.write("Pour commencer, vous pouvez vous rendre sur la page 1. Chatbot avec Brutus.")
 st.write("A vos prompts ! !")
