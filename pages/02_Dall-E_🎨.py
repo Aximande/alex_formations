711a56de36c6b97b85608ad73e2151079ae06b26
alexandre lavallee <lavallee.alexx@gmail.com>
2024-04-23 02:31:47 +0200

    update dalle-3 creation

--- pages/02_Dall-E_🎨.py
+++ pages/02_Dall-E_🎨.py
@@ -7,14 +7,11 @@ from uuid import uuid4
 import requests
 from io import BytesIO
 from langchain.chat_models import ChatOpenAI
-
-# Callbacks for observability
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from langchain_community.utilities.dalle_image_generator import DallEAPIWrapper
-
 
 # Set and load environment variables for Langchain and OpenAI
 load_dotenv()
@@ -25,19 +22,17 @@ unique_id = uuid4().hex[0:8]  # Generating a unique ID for this session
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_PROJECT"] = f"Project - {unique_id}"
 os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
-os.environ["LANGCHAIN_API_KEY"] = "ls__5d5b2266e1ac446a85974cd1db8349c5"  # Replace with your actual API key - to change for mor esecurity
+os.environ["LANGCHAIN_API_KEY"] = "ls__5d5b2266e1ac446a85974cd1db8349c5"  # Replace with your actual API key - to change for more security
 
-
-# Function to generate an image using Dall-E based on a given description
-def generate_dalle_image(description):
+# Function to generate an image using Dall-E based on a given description and size
+def generate_dalle_image(description, size):
     llm = ChatOpenAI(temperature=0.9)
     prompt = PromptTemplate(
         input_variables=["image_desc"],
         template="Generate a detailed prompt to generate an image based on the following description: {image_desc}",
     )
     chain = LLMChain(llm=llm, prompt=prompt)
-
-    image_url = DallEAPIWrapper(model='dall-e-3',size=size).run(chain.run(description))
+    image_url = DallEAPIWrapper(model='dall-e-3', size=size).run(chain.run(description))
     response = requests.get(image_url)
     img = Image.open(BytesIO(response.content))
     return img
@@ -47,10 +42,7 @@ st.set_page_config(page_title="DALL-E Image Generator", page_icon=":art:", layou
 st.title("DALL-E Image Generator 🎨")
 
 # Display the logo at the top of the page
-st.image(
-    Image.open("static/brutAI_logo_noir_background.png"),
-    width=200
-)
+st.image(Image.open("static/brutAI_logo_noir_background.png"), width=200)
 
 with st.container():
     col1, col2, col3 = st.columns([1, 2, 1])
